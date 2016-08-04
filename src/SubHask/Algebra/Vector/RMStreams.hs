8fc85b016c97a5f41d12f057ef5adfa922fe63db
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2016-08-04 17:13:26 +0200

    typechecks but triggers GHC-Bug

--- src/SubHask/Algebra/Vector/RMStreams.hs
+++ src/SubHask/Algebra/Vector/RMStreams.hs
@@ -8,10 +8,11 @@ module SubHask.Algebra.Vector.RMStreams where
 
 import Control.Monad
 import Control.Monad.Identity
+import Control.Monad.ST
 import Data.Functor
-import Prelude ((.), ($))
+import Prelude ((.), ($), Int)
 
-import SubHask.Internal.Prelude
+--import SubHask.Internal.Prelude
 
 -- | Primitive for Streaming operations on index i with value a
 data Step i a = Yield a i -- ^ replace index with a
@@ -24,16 +25,16 @@ instance Functor (Step i) where
     fmap _ Done        = Done
 
 -- | The MStream composed of a next action in the monad m, the index it works on and the size of the structure
-data MStream m i a = MStream (i -> m (Step i a)) {-# UNBOX #-} !i {-# UNBOX #-} !Int
+data MStream m s i a = MStream (i -> m (Step i a)) {-# UNBOX #-} !i {-# UNBOX #-} !Int
 
 
 -- | stream . unstream gets fused away, chaining the MStream i a -> MStream i a functions.
 class (Monad m) => Streamable m s i where
-        stream :: s i a -> MStream m i a
-        unstream :: MStream m i a -> m (s i a)
+        stream :: s i a -> MStream m s i a
+        unstream :: MStream m s i a -> m (s i a)
 
 {-# RULES
-"stream/unstream" forall (t :: forall i a. MStream Identity i a). stream (unstream t) = t
+"stream/unstream" forall (t :: forall s i a. MStream Identity s i a). stream (unstream t) = t
   #-}
 
 -- | Wrapper for signaling a creation of a value
@@ -55,34 +56,34 @@ class Recycleable r where
 --   unstream and clone with the fill-based definition which then gets
 --   optimised away by further rules.
 class (Monad m, Streamable m s i, Recycleable s, m ~ ST i) => RMStreams m s i where
-        fill :: MStream m i a -> New s i a
+        fill :: MStream m s i a -> New s i a
 
         {-# INLINE unstream_ #-}
-        unstream_ :: MStream m i a-> m (s i a)
+        unstream_ :: MStream m s i a-> m (s i a)
         unstream_ s = return $ new (fill s)
 
         {-# INLINE clone_ #-}
         clone_ :: s i a -> New s i a
-        clone_ s = fill ((stream :: s i a -> MStream m i a) s)
+        clone_ s = fill ((stream :: s i a -> MStream m s i a) s)
 
         {-# INLINE transform #-}
-        transform :: (forall m. Monad m => MStream m i a -> MStream m i a) -> New s i a -> New s i a
+        transform :: (forall m. Monad m => MStream m s i a -> MStream m s i a) -> New s i a -> New s i a
         transform f (New init) = New $ do 
                                        v <- init
-                                       (unstream_ :: MStream (ST i) i a -> ST i (s i a)) (f ((stream :: s i a -> MStream (ST i) i a) v))
+                                       (unstream_ :: MStream (ST i) s i a -> ST i (s i a)) (f ((stream :: s i a -> MStream (ST i) s i a) v))
 
         -- | functions that do not change the type can be done inplace. Definition is id but used in GHC-Rules.
         {-# INLINE inplace #-}
-        inplace :: (forall m. Monad m => MStream m i a -> MStream m i a) -> MStream m i a -> MStream m i a
+        inplace :: (forall m. Monad m => MStream m s i a -> MStream m s i a) -> MStream m s i a -> MStream m s i a
         inplace f = f
 
-{-# RULES
-"unstream/fill_unstream" forall (t :: (Streamable m s i, RMStreams m s i) => MStream m i a). unstream t = unstream_ t :: (Streamable m s i, RMStreams m s i) => m (s i a)
+{- # RULES
+"unstream/fill_unstream" forall (t :: (Streamable m s i, RMStreams m s i) => MStream m s i a). unstream t = unstream_ t :: (Streamable m s i, RMStreams m s i) => m (s i a)
 "clone/fill_clone" forall (t :: (Recycleable s, RMStreams m s i) => s i a). clone t = clone_ t :: (Recycleable s, RMStreams m s i) => New s i a
 "fusion" forall s. stream (new (fill s)) = s
 "recycling" forall p. fill (stream (new p)) = p
-"inplace" forall (f :: forall m. Monad m => MStream m i a -> MStream m i a) p. fill (inplace f (stream (new p))) = transform (f :: forall m. Monad m => MStream m i a -> MStream m i a) p
-"uninplace" forall (f :: forall m. MStream m i a -> MStream m i a) p. stream (new (transform f p)) = inplace f (stream (new p))
-"inplace2" forall (f :: forall m. MStream m i a -> MStream m i a) (g :: forall m. MStream m i a -> MStream m i a) (s :: RMStreams m r i => MStream m i a). inplace f (inplace g s) = inplace (f . g) s :: RMStreams m r i => MStream m i a
-"mfusion" forall (f :: forall m. MStream m i a -> MStream m i a) (g :: forall m. MStream m i a -> MStream m i a) (p :: New s i a). transform f (transform g p) = transform (f . g :: forall m. MStream m i a -> MStream m i a) p :: New s i a
-  #-}
+"inplace" forall (f :: forall m. Monad m => MStream m s i a -> MStream m s i a) p. fill (inplace f (stream (new p))) = transform (f :: forall m. Monad m => MStream m s i a -> MStream m s i a) p
+"uninplace" forall (f :: forall m. Monad m => MStream m s i a -> MStream m s i a) (p :: RMStreams m s i => New s i a). stream (new (transform f p)) = inplace f (stream (new p)) :: RMStreams m s i => MStream m s i a
+"inplace2" forall (f :: forall m. MStream m s i a -> MStream m s i a) (g :: forall m. MStream m s i a -> MStream m s i a) (s :: RMStreams m r i => MStream m s i a). inplace f (inplace g s) = inplace ((f . g) :: RMStreams m r i => forall m . MStream m s i a -> MStream m s i a) s :: MStream m s i a
+"mfusion" forall (f :: forall m. MStream m s i a -> MStream m s i a) (g :: forall m. MStream m s i a -> MStream m s i a) (p :: New s i a). transform f (transform g p) = transform (f . g :: forall m. MStream m s i a -> MStream m s i a) p :: New s i a
+  # -}
